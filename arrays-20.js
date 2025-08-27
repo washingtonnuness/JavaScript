414d73666a4bbea6e2cb0fd341af863eb7800772
washingtonnuness <washingtonnuness@gmail.com>
2025-08-27 14:41:31 -0300

    Create arrays-20.js

---
+++ arrays-20.js
@@ -0,0 +1,34 @@
+/*
+20.Criar um array de arrays com 5 produtos pré-definidos (descritos abaixo) e
+calcular o valor total da compra. Depois, aplicar um cupom de 20% de desconto
+e mostrar o valor final. Ordem dos valores: [‘Nome’, ‘Preço’, ‘Quantidade’].
+*/
+
+let produtos = [
+  ["Camisa", 44.9, 5],
+  ["Bermuda", 69.9, 3],
+  ["Tenis", 179.9, 2],
+  ["Calça", 129.9, 2],
+  ["Sapato", 239.9, 1],
+];
+
+let somaItens = 0;
+let somaValor = 0;
+let itens = 0;
+let somaValorDesconto = 0;
+
+const desconto = 0.2; // Valor 20 / 100 = 0,2% ;
+
+console.log("Array original", produtos);
+
+const produtosMap = produtos.map((produtos) => {
+  produtos[1] = produtos[2] * produtos[1];
+  somaValor += produtos[1];
+
+  return produtos;
+});
+somaValorDesconto = somaValor * desconto;
+
+console.log("Array original", produtosMap);
+console.log("Total da compra: R$", somaValor);
+console.log("Valor com desconto (20% OFF): R$", somaValorDesconto);
